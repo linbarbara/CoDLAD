99ca3a7f4460eabe360eb9e837b1380dd3fdf7db
linling <162523798+linbarbara@users.noreply.github.com>
2026-01-27 21:14:50 +0800

    Delete train_all.py

--- train_all.py
+++
@@ -1,8 +0,0 @@
-from pretrain_mask_vae import main_pretrain
-from classifier import main_train_classifier
-
-if __name__ == '__main__':
-    for i in range(0,10):
-        main_pretrain(i)
-        main_train_classifier(i)
-
